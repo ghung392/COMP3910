5965df3028d472d7f909b4b1fe44635fded2c236
Gabriel Hung <ghung392@live.ca>
2014-10-11 21:15:57 -0700

    Added Javadoc to Employee Model

--- JavaSource/com/corejsf/Model/EmployeeModel.java
+++ JavaSource/com/corejsf/Model/EmployeeModel.java
@@ -7,27 +7,48 @@ import ca.bcit.infosys.employee.Employee;
  * @version 1.0
  */
 public class EmployeeModel extends Employee {
+    /** Employee password */
     private String password;
+    /** Employee admin status */
     private boolean admin;
-    
+    /**
+     * Constructor to instantiate an employee
+     * @param empName employee name
+     * @param number employee number
+     * @param id employee username
+     * @param isSuper boolean to determine whether user has admin status
+     * @param pass employee password
+     */
     public EmployeeModel(final String empName, final int number, final String id, final boolean isSuper, final String pass) {
         super(empName, number, id);
         admin = isSuper;
         password = pass;
     }
-    
+    /**
+     * Getter for password
+     * @return employee password
+     */
     public String getPassword() {
         return password;
     }
-    
+    /**
+     * Setter for password
+     * @param password new password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    /**
+     * Setter for admin status
+     * @param newValue new admin status
+     */
     public void setAdmin(boolean newValue) {
         admin = newValue;
     }
-    
+    /**
+     * Getter for admin status
+     * @return current admin status
+     */
     public boolean isAdmin() {
         return admin;
     }
