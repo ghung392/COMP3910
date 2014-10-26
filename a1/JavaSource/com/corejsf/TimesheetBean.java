b1ffdc0140f5065d59684d81b26e79346427723b
ama <starangelam@gmail.com>
2014-10-26 15:36:28 -0700

    add java doc and fix checktyle errors

--- a1/JavaSource/com/corejsf/TimesheetBean.java
+++ a1/JavaSource/com/corejsf/TimesheetBean.java
@@ -15,122 +15,187 @@ import com.corejsf.Access.TimesheetManager;
 import com.corejsf.Model.EmployeeModel;
 import com.corejsf.Model.TimesheetModel;
 
+/**
+ * TimesheetBean is a CDI bean that holds relevant information and business
+ * logic to interact with JSF XHTML presentation layer.
+ *
+ */
 @Named("timesheetData")
 @SessionScoped
 public class TimesheetBean implements Serializable {
 
-	@Inject
-	private TimesheetManager timesheetManager;
-	@Inject
-	private EmployeeInfo employeeSession;
+    /** Manages access and persistence of timesheets in data layer. */
+    @Inject
+    private TimesheetManager timesheetManager;
+    /** CDI manages manages employee related data over a session. */
+    @Inject
+    private EmployeeInfo employeeSession;
 
-	private List<TimesheetModel> allTimesheets;
-	private TimesheetModel timesheet;
-	private Date currEndWeek;
-	private boolean saveSuccess = false;
-	private boolean showMessages = false;
-	private List<String> errorMessages = new ArrayList<String>();
-	
-	public boolean getSaveSuccess() {
-		return saveSuccess;
-	}
+    /** All timesheets belonging to current employee. */
+    private List<TimesheetModel> allTimesheets;
+    /** End of week of the timesheet to view or viewing. */
+    private Date currEndWeek;
+    /** A timesheet belonging to current employee, specified by currEndWeek. */
+    private TimesheetModel timesheet;
+    /** Flag indicate whether successfully saved timesheet. */
+    private boolean saveSuccess = false;
+    /** Flag deciding whether to show message areas. */
+    private boolean showMessages = false;
+    /** List of error messages related to saving timesheet. */
+    private List<String> errorMessages = new ArrayList<String>();
 
-	public boolean getShowMessages() {
-		return showMessages;
-	}
-	
-	public List<String> getErrorMessages(){
-		return errorMessages;
-	}
+    /**
+     * Get saveSuccess flag.
+     * @return whether timesheet successfully saved.
+     */
+    public boolean getSaveSuccess() {
+        return saveSuccess;
+    }
 
-	public List<TimesheetModel> getAllTimesheets() {
-		// refresh timesheetList only if not initialized, empty or current
-		// employee changed.
-		if (allTimesheets == null) {
-			refreshTimesheetList();
-		} else {
-			if (allTimesheets.size() == 0) {
-				refreshTimesheetList();
-			} else {
-				EmployeeModel reference = (EmployeeModel) allTimesheets.get(0).getEmployee();
-				if (!reference.equals(employeeSession.getCurrentEmployee())) {
-					refreshTimesheetList();
-				}
+    /**
+     * Get showMessages flag.
+     * @return whether show message error on timesheet page.
+     */
+    public boolean getShowMessages() {
+        return showMessages;
+    }
 
-			}
-		}
+    /**
+     * Get list of error messages related to timesheet saving.
+     * @return list of timesheet saving error messages.
+     */
+    public List<String> getErrorMessages() {
+        return errorMessages;
+    }
 
-		return allTimesheets;
-	}
+    /**
+     * Get all timesheets belonging to currently logged in employee.
+     * @return all timesheets belonging to currently logged in employee.
+     */
+    public List<TimesheetModel> getAllTimesheets() {
+        // refresh timesheetList only if not initialized, empty or current
+        // employee changed.
+        if (allTimesheets == null) {
+            refreshTimesheetList();
+        } else {
+            if (allTimesheets.size() == 0) {
+                refreshTimesheetList();
+            } else {
+                EmployeeModel reference = (EmployeeModel) allTimesheets.get(0)
+                        .getEmployee();
+                if (!reference.equals(employeeSession.getCurrentEmployee())) {
+                    refreshTimesheetList();
+                }
 
-	public TimesheetModel getTimesheet() {
-		// refresh timesheet only if not initialized or currEndWeek changed
-		final boolean isSameCurrEndWeek = timesheet != null && timesheet.isSameWeekEnd(currEndWeek);
-		if (timesheet == null || !isSameCurrEndWeek) {
-			refreshTimesheet();
-		}
+            }
+        }
 
-		return timesheet;
-	}
+        return allTimesheets;
+    }
 
-	private void refreshTimesheetList() {
-		System.out.println("Refreshing employee's TimesheetList");
-		Employee currEmployee = employeeSession.getCurrentEmployee();
-		allTimesheets = timesheetManager.getTimesheets(currEmployee);
-	}
+    /**
+     * Get specified week's timesheet of current employee. Week dictated by
+     * currEndWeek property.
+     * @return a timesheet.
+     */
+    public TimesheetModel getTimesheet() {
+        // refresh timesheet only if not initialized or currEndWeek changed
+        final boolean isSameCurrEndWeek = timesheet != null
+                && timesheet.isSameWeekEnd(currEndWeek);
+        if (timesheet == null || !isSameCurrEndWeek) {
+            refreshTimesheet();
+        }
 
-	private void refreshTimesheet() {
-		System.out.println("Refreshing current timesheet.");
-		Employee currEmployee = employeeSession.getCurrentEmployee();
-		TimesheetModel newSheet = timesheetManager.getTimesheet(currEmployee, currEndWeek);
-		timesheet = newSheet;
-	}
+        return timesheet;
+    }
 
-	public String addTimesheet() {
-		boolean isValid = true;
+    /**
+     * Update current employee's timesheet list from data layer.
+     */
+    private void refreshTimesheetList() {
+        System.out.println("Refreshing employee's TimesheetList");
+        Employee currEmployee = employeeSession.getCurrentEmployee();
+        allTimesheets = timesheetManager.getTimesheets(currEmployee);
+    }
 
-		errorMessages.clear();
-		showMessages = true;
-		
-		timesheet.trimmedDetails();
-		
-		if (!timesheet.isValid()) {
-			isValid = false;
-			saveSuccess = false;
-			errorMessages.add("Work hours must add up to 40.");
-			System.out.println("Work hours must add up to 40.");
-		}
-		if (!timesheet.isRowsValid()) {
-			isValid = false;
-			saveSuccess = false;
-			errorMessages.add("A combination of project id + workpage must be filled and unique for each row.");
-			System.out.println("A combination of project id + workpage must be filled and unique for each row.");			
-		}
-		if (isValid) {
-			timesheetManager.saveTimesheet(timesheet);
-			saveSuccess = true;
-			System.out.println("Saving timesheet");
-		}
+    /**
+     * Update current employee's timesheet from data layer.
+     */
+    private void refreshTimesheet() {
+        System.out.println("Refreshing current timesheet.");
+        Employee currEmployee = employeeSession.getCurrentEmployee();
+        TimesheetModel newSheet = timesheetManager.getTimesheet(currEmployee,
+                currEndWeek);
+        timesheet = newSheet;
+    }
 
-		return null;
-	}
-	
-	public String addRow() {
-	    timesheet.addRow();
-	    return null;
-	}
+    /**
+     * Validate and save timesheet to data layer.
+     *
+     * @return navigation outcome - return to same page.
+     */
+    public String addTimesheet() {
+        boolean isValid = true;
 
-	public String currTimesheet() {
-		showMessages = false;
-		currEndWeek = TimesheetModel.getCurrDate();
-		return "createTimesheet";
-	}
+        errorMessages.clear();
+        showMessages = true;
 
-	public String viewTimesheet(final Date endWeek) {
-		currEndWeek = endWeek;
+        timesheet.trimmedDetails();
 
-		// TODO return proper view page of timesheet
-		return "createTimesheet";
-	}
+        if (!timesheet.isValid()) {
+            isValid = false;
+            saveSuccess = false;
+            errorMessages.add("Work hours must add up to 40.");
+            System.out.println("Work hours must add up to 40.");
+        }
+        if (!timesheet.isRowsValid()) {
+            isValid = false;
+            saveSuccess = false;
+            errorMessages.add("A combination of project id & workpage must be"
+                    + "filled and unique for each row.");
+            System.out.println("A combination of project id & workpage must be"
+                    + "filled and unique for each row.");
+        }
+        if (isValid) {
+            timesheetManager.saveTimesheet(timesheet);
+            saveSuccess = true;
+            System.out.println("Saving timesheet");
+        }
+
+        return null;
+    }
+
+    /**
+     * Add a new timesheetRow to timesheet table.
+     * @return navigation outcome - return to same page.
+     */
+    public String addRow() {
+        timesheet.addRow();
+        return null;
+    }
+
+    /**
+     * Go to Fill timesheet page to create / edit current week's timesheet.
+     * Set currEndWeek to current week's Friday.
+     * @return navigation outcome - createTimesheet page.
+     */
+    public String currTimesheet() {
+        showMessages = false;
+        currEndWeek = TimesheetModel.getCurrDate();
+        return "createTimesheet";
+    }
+
+    /**
+     * Go to View a past timesheet page. Set currEndWeek to specified
+     * week's Friday.
+     * @param endWeek specified week's Friday.
+     * @return navigation outcome - viewTimesheet page.
+     */
+    public String viewTimesheet(final Date endWeek) {
+        currEndWeek = endWeek;
+
+        // TODO return proper view page of timesheet
+        return "createTimesheet";
+    }
 
 }
