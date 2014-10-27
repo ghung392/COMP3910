53c8530e2b1e575c14038eab62e605d119bace1d
ama <starangelam@gmail.com>
2014-10-26 18:37:31 -0700

    checkstyle error fix

--- a1/JavaSource/com/corejsf/Access/TimesheetManager.java
+++ a1/JavaSource/com/corejsf/Access/TimesheetManager.java
@@ -17,115 +17,153 @@ import com.corejsf.Model.EmployeeModel;
 import com.corejsf.Model.TimesheetModel;
 import com.corejsf.Model.TimesheetRowModel;
 
+/**
+ * Manages the access of timesheet data in the data layer.
+ */
 @Named("timesheetManager")
 @ApplicationScoped
 public class TimesheetManager {
 
-	private List<TimesheetModel> timesheetCollection;
-	private EmployeeTracker employeeManager = new EmployeeTracker();
+    /** Replacement of database, holding all timesheets. */
+    private List<TimesheetModel> timesheetCollection;
+    /** Manager that control access of employee data from data layer. */
+    private EmployeeTracker employeeManager = new EmployeeTracker();
 
-	public TimesheetManager() {
-		timesheetCollection = new LinkedList<TimesheetModel>();
-		populateTimesheetCollection();
-	}
+    /**
+     * Construction a TimesheetManager object. Initializing timesheetColleciton
+     * list and populate it with some sample data.
+     */
+    public TimesheetManager() {
+        timesheetCollection = new LinkedList<TimesheetModel>();
+        populateTimesheetCollection();
+    }
 
-	public List<TimesheetModel> getTimesheets() {
-		return timesheetCollection;
-	}
+    /**
+     * @return all timesheets stored.
+     */
+    public List<TimesheetModel> getTimesheets() {
+        return timesheetCollection;
+    }
 
-	public List<TimesheetModel> getTimesheets(Employee e) {
-		if (e == null) {
+    /**
+     * Get all timesheet belonging to given employee.
+     * @param e
+     *            owner of timesheet.
+     * @return all timesheet belonging to given employee
+     */
+    public List<TimesheetModel> getTimesheets(final Employee e) {
+        if (e == null) {
             return null;
         }
 
-		List<TimesheetModel> employeeTimesheets = new LinkedList<TimesheetModel>();
-		for (TimesheetModel t : timesheetCollection) {
-			if (e.equals(t.getEmployee())) {
-				employeeTimesheets.add(t);
-			}
-		}
-		return employeeTimesheets;
-	}
+        List<TimesheetModel> empTimesheets = new LinkedList<TimesheetModel>();
+        for (TimesheetModel t : timesheetCollection) {
+            if (e.equals(t.getEmployee())) {
+                empTimesheets.add(t);
+            }
+        }
+        return empTimesheets;
+    }
 
-	public TimesheetModel getTimesheet(final Employee e, final Date weekEnd) {
-		List<TimesheetModel> employeeTimesheets = getTimesheets(e);
-		TimesheetModel timesheet = null;
+    /**
+     * Get timesheet belong to a given employee and given week.
+     * @param e
+     *            timesheet's owner
+     * @param weekEnd
+     *            timesheet's week end day
+     * @return a timesheet of a given employee and given week.
+     */
+    public TimesheetModel getTimesheet(final Employee e, final Date weekEnd) {
+        List<TimesheetModel> employeeTimesheets = getTimesheets(e);
+        TimesheetModel timesheet = null;
 
-		for (TimesheetModel t : employeeTimesheets) {
-			if (t.isSameWeekEnd(weekEnd)) {
-				timesheet = t;
-				System.out.println("Found timesheet for " + weekEnd.toString());
-			}
-		}
-		if (timesheet == null) {
-			timesheet = new TimesheetModel(e);
-			System.out.println("Create timesheet for " + timesheet.getEndWeek().toString());
-		}
+        for (TimesheetModel t : employeeTimesheets) {
+            if (t.isSameWeekEnd(weekEnd)) {
+                timesheet = t;
+                System.out.println("Found timesheet for " + weekEnd.toString());
+            }
+        }
+        if (timesheet == null) {
+            timesheet = new TimesheetModel(e);
+            System.out.println("Create timesheet for "
+                    + timesheet.getEndWeek().toString());
+        }
 
-		return timesheet;
-	}
+        return timesheet;
+    }
 
-	public void saveTimesheet(final TimesheetModel timesheet) {
-		if (timesheet == null) {
-		    System.out.println("Error: null passed in as timesheet to be saved.");
+    /**
+     * Saves a timesheet - replace if exist in data storage, if no record found
+     * create a new one.
+     * @param timesheet
+     *            timesheet to store
+     */
+    public void saveTimesheet(final TimesheetModel timesheet) {
+        if (timesheet == null) {
+            System.out.println("Error: null passed as timesheet to be saved.");
             return;
         }
 
-		boolean saved = false;
+        boolean saved = false;
 
-		final int size = timesheetCollection.size();
-		for (int i = 0; i < size; i++) {
-			// if timesheet found in timesheetCollection, replace with new
-			if (timesheet.equals(timesheetCollection.get(i))) {
-				saved = true;
-				timesheetCollection.set(i, timesheet);
-				System.out.println("Timesheet replaced.");
-				break;
-			}
-		}
-		// add timesheet only if it is not found in collection
-		if (!saved) {
-			timesheetCollection.add(timesheet);
-			System.out.println("Timesheet added.");
-		}
-	}
+        final int size = timesheetCollection.size();
+        for (int i = 0; i < size; i++) {
+            // if timesheet found in timesheetCollection, replace with new
+            if (timesheet.equals(timesheetCollection.get(i))) {
+                saved = true;
+                timesheetCollection.set(i, timesheet);
+                System.out.println("Timesheet replaced.");
+                break;
+            }
+        }
+        // add timesheet only if it is not found in collection
+        if (!saved) {
+            timesheetCollection.add(timesheet);
+            System.out.println("Timesheet added.");
+        }
+    }
 
-	private void populateTimesheetCollection() {
-		final int P1 = 132;
+    /**
+     * Populate data storage with sample Timesheets.
+     */
+    private void populateTimesheetCollection() {
+        final int p1 = 132;
+        final int h = 8;
 
-		final List<TimesheetRow> e1Rows = new LinkedList<TimesheetRow>();
+        final List<TimesheetRow> e1Rows = new LinkedList<TimesheetRow>();
 
-		TimesheetRowModel row;
-		BigDecimal[] h1 = { null, null, new BigDecimal(8), new BigDecimal(6), null,
-				new BigDecimal(8), new BigDecimal(8) };
-		row = new TimesheetRowModel(P1, "AA123", h1, "");
-		e1Rows.add(row);
+        TimesheetRowModel row;
+        BigDecimal[] h1 = {null, null, new BigDecimal(h), null, null,
+                new BigDecimal(h), new BigDecimal(h) };
+        row = new TimesheetRowModel(p1, "AA123", h1, "");
+        e1Rows.add(row);
 
-		BigDecimal[] h2 = { null, null, null, null, new BigDecimal(2), null, new BigDecimal(8) };
-		row = new TimesheetRowModel(P1, "AA122", h2, "");
-		e1Rows.add(row);
+        BigDecimal[] h2 = {null, null, null, null, new BigDecimal(h), null,
+                new BigDecimal(h) };
+        row = new TimesheetRowModel(p1, "AA122", h2, "");
+        e1Rows.add(row);
 
-		try {
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-			Date end = sdf.parse("10/10/2014");
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            Date end = sdf.parse("10/10/2014");
 
-			final List<EmployeeModel> elist = employeeManager.getEmployees();
-			if (elist != null && elist.size() >= 2) {
-				final Employee e1 = elist.get(0);
-				final TimesheetModel t1 = new TimesheetModel(e1, end, e1Rows);
-				saveTimesheet(t1);
+            final List<EmployeeModel> elist = employeeManager.getEmployees();
+            if (elist != null && elist.size() >= 2) {
+                final Employee e1 = elist.get(0);
+                final TimesheetModel t1 = new TimesheetModel(e1, end, e1Rows);
+                saveTimesheet(t1);
 
-				final Employee e2 = elist.get(1);
-				final TimesheetModel t2 = new TimesheetModel(e2, end, e1Rows);
-				saveTimesheet(t2);
-			} else {
-				System.out.println("no user!");
-			}
+                final Employee e2 = elist.get(1);
+                final TimesheetModel t2 = new TimesheetModel(e2, end, e1Rows);
+                saveTimesheet(t2);
+            } else {
+                System.out.println("no user!");
+            }
 
-		} catch (ParseException e) {
-			System.out.println("ERROR: cannot parse strng date");
-			e.printStackTrace();
-		}
-	}
+        } catch (ParseException e) {
+            System.out.println("ERROR: cannot parse strng date");
+            e.printStackTrace();
+        }
+    }
 
 }
--- a1/JavaSource/com/corejsf/Model/TimesheetRowModel.java
+++ a1/JavaSource/com/corejsf/Model/TimesheetRowModel.java
@@ -4,97 +4,173 @@ import java.math.BigDecimal;
 
 import ca.bcit.infosys.timesheet.TimesheetRow;
 
+/**
+ * Extending TimesheetRow class, representing a single row on a timesheet
+ * containing identifier and hours per day.
+ */
 public class TimesheetRowModel extends TimesheetRow {
-	
-	public TimesheetRowModel() {
-		super();
-	}
-	
-	public TimesheetRowModel(final int id, final String wp,
+
+    /**
+     * Creates a TimesheetRowModel object.
+     */
+    public TimesheetRowModel() {
+        super();
+    }
+
+    /**
+     * Creates a TimesheetRowModel object with all fields set. Used to create
+     * sample data.
+     * @param id project id
+     * @param wp work package number (alphanumeric)
+     * @param hours number of hours charged for each day of week.
+     *      null represents ZERO
+     * @param comments any notes with respect to this work package charges
+     */
+    public TimesheetRowModel(final int id, final String wp,
             final BigDecimal[] hours, final String comments) {
-		super(id, wp, hours, comments);
-	}
+        super(id, wp, hours, comments);
+    }
 
-	public BigDecimal getHourMon() {
-		return getHour(TimesheetRow.MON);
-	}
+    /**
+     * @return Monday's hour on this row.
+     */
+    public BigDecimal getHourMon() {
+        return getHour(TimesheetRow.MON);
+    }
 
-	public BigDecimal getHourTue() {
-		return getHour(TimesheetRow.TUE);
-	}
+    /**
+     * @return Tuesday's hour on this row.
+     */
+    public BigDecimal getHourTue() {
+        return getHour(TimesheetRow.TUE);
+    }
 
-	public BigDecimal getHourWed() {
-		return getHour(TimesheetRow.WED);
-	}
+    /**
+     * @return Wednesday's hour on this row.
+     */
+    public BigDecimal getHourWed() {
+        return getHour(TimesheetRow.WED);
+    }
 
-	public BigDecimal getHourThur() {
-		return getHour(TimesheetRow.THU);
-	}
+    /**
+     * @return Thursday's hour on this row.
+     */
+    public BigDecimal getHourThur() {
+        return getHour(TimesheetRow.THU);
+    }
 
-	public BigDecimal getHourFri() {
-		return getHour(TimesheetRow.FRI);
-	}
+    /**
+     * @return Friday's hour on this row.
+     */
+    public BigDecimal getHourFri() {
+        return getHour(TimesheetRow.FRI);
+    }
 
-	public BigDecimal getHourSat() {
-		return getHour(TimesheetRow.SAT);
-	}
+    /**
+     * @return Saturday's hour on this row.
+     */
+    public BigDecimal getHourSat() {
+        return getHour(TimesheetRow.SAT);
+    }
 
-	public BigDecimal getHourSun() {
-		return getHour(TimesheetRow.SUN);
-	}
+    /**
+     * @return Sunday's hour on this row.
+     */
+    public BigDecimal getHourSun() {
+        return getHour(TimesheetRow.SUN);
+    }
 
-	public void setHourMon(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.MON, hour);
-	}
+    /**
+     * Set hour on Monday.
+     * @param hour number of hours worked
+     */
+    public void setHourMon(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.MON, hour);
+    }
 
-	public void setHourTue(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.TUE, hour);
-	}
+    /**
+     * Set hour on Tuesday.
+     * @param hour number of hours worked
+     */
+    public void setHourTue(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.TUE, hour);
+    }
 
-	public void setHourWed(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.WED, hour);
-	}
+    /**
+     * Set hour on Wednesday.
+     * @param hour number of hours worked
+     */
+    public void setHourWed(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.WED, hour);
+    }
 
-	public void setHourThur(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.THU, hour);
-	}
+    /**
+     * Set hour on Thursday.
+     * @param hour number of hours worked
+     */
+    public void setHourThur(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.THU, hour);
+    }
 
-	public void setHourFri(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.FRI, hour);
-	}
+    /**
+     * Set hour on Friday.
+     * @param hour number of hours worked
+     */
+    public void setHourFri(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.FRI, hour);
+    }
 
-	public void setHourSat(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.SAT, hour);
-	}
+    /**
+     * Set hour on Saturday.
+     * @param hour number of hours worked
+     */
+    public void setHourSat(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.SAT, hour);
+    }
 
-	public void setHourSun(final BigDecimal hour) {
-		setHourOnDay(TimesheetRow.SUN, hour);
-	}
-	
-	private void setHourOnDay(final int day, final BigDecimal hour) {
-		try {
-			setHour(day, hour);
-		}
-		catch(IllegalArgumentException e) {
-			// catch error
-		}
-	}
+    /**
+     * Set hour on Sunday.
+     * @param hour number of hours worked
+     */
+    public void setHourSun(final BigDecimal hour) {
+        setHourOnDay(TimesheetRow.SUN, hour);
+    }
 
-	public boolean isDuplicate(final TimesheetRowModel reference) {
-		boolean isDuplicate = true;
+    /**
+     * Set hour on a given day.
+     * @param day day to set hours for
+     * @param hour hour number of hours worked
+     */
+    private void setHourOnDay(final int day, final BigDecimal hour) {
+        try {
+            setHour(day, hour);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Failed to set hour.");
+        }
+    }
 
-		final String wp1 = getWorkPackage();
-		final String wp2 = reference.getWorkPackage();
+    /**
+     * Check if row is a duplicate of given row. Definition of duplicate is
+     * when two rows has exact same combination of projectID + WorkPackage.
+     * @param reference row to be checked against.
+     * @return whether given row is a duplicate of current row
+     */
+    public final boolean isDuplicate(final TimesheetRowModel reference) {
+        boolean isDuplicate = true;
 
-		if ((wp1 != null && wp1.length() > 0) && (wp2 != null && wp2.length() > 0)) {
-			final String id1 = getProjectID() + wp1;
-			final String id2 = reference.getProjectID() + wp2;
-			if (!id1.equals(id2)) {
-				isDuplicate = false;
-			}
-		}
+        final String wp1 = getWorkPackage();
+        final String wp2 = reference.getWorkPackage();
 
-		return isDuplicate;
-	}
+        if ((wp1 != null && wp1.length() > 0)
+                && (wp2 != null && wp2.length() > 0)) {
+            final String id1 = getProjectID() + wp1;
+            final String id2 = reference.getProjectID() + wp2;
+            if (!id1.equals(id2)) {
+                isDuplicate = false;
+            }
+        }
+
+        return isDuplicate;
+    }
 
 }
